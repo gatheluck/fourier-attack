4b76b4faae3725662c454fe6c9daab90bdfaae79
gatheluck <gatheluck+tech@gmail.com>
2021-01-26 14:27:03 +0900

    test: add adv trained model to test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pathlib
+import numpy as np
+import random
 from typing import Final
 
 import pytest
@@ -7,9 +9,17 @@ import torchvision
 
 CIFAR10_ROOT: Final = pathlib.Path("data/cifar10")
 PRETRAONED_WEIGHT = pathlib.Path("tests/weight/cifar10_resnet50.pth")
+PRETRAONED_WEIGHT_ADV = pathlib.Path("tests/weight/cifar10_resnet50_adv.pth")
 CIFAR10_MEAN: Final = [0.49139968, 0.48215841, 0.44653091]
 CIFAR10_STD: Final = [0.24703223, 0.24348513, 0.26158784]
 BATCH_SIZE = 16
+
+seed = 0
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cudnn.deterministic = True
 
 
 @pytest.fixture
@@ -49,7 +59,14 @@ def denormalize_cifar10_loader():
 
 
 @pytest.fixture
-def pretrained_resnet50():
-    model = torchvision.models.resnet50(pretrained=False)
+def pretrained_cifar10_resnet50():
+    model = torchvision.models.resnet50(pretrained=False, num_classes=10)
     model.load_state_dict(torch.load(PRETRAONED_WEIGHT))
+    return model.eval()
+
+
+@pytest.fixture
+def pretrained_cifar10_resnet50_adv():
+    model = torchvision.models.resnet50(pretrained=False, num_classes=10)
+    model.load_state_dict(torch.load(PRETRAONED_WEIGHT_ADV))
     return model.eval()
