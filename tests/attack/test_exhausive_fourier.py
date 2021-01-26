117d3aa84bf20f1b17ee81a83acc98ca85e6e877
gatheluck <gatheluck+tech@gmail.com>
2021-01-26 23:23:34 +0900

    test: add test for forward.

--- tests/attack/test_exhausive_fourier.py
+++ tests/attack/test_exhausive_fourier.py
@@ -11,6 +11,7 @@ class TestExhausiveFourierAttack:
     def test__run(self, pretrained_cifar10_resnet50, cifar10_stats, denormalize_cifar10_loader):
         devices = {"cuda"}
         output_root: Final = pathlib.Path("logs/test/")
+        output_root.mkdir()
 
         model = pretrained_cifar10_resnet50
         criterion_func = torch.nn.functional.cross_entropy
@@ -33,15 +34,45 @@ class TestExhausiveFourierAttack:
 
                 x_adv = (pixel_x + perb) / 255.
                 perb_ = (perb / 255.) + 0.5
-                torchvision.utils.save_image(x_adv, output_root / "exhausive_fourier.png")
-                torchvision.utils.save_image(perb_, output_root / "perturbation.png")
+                torchvision.utils.save_image(x_adv, output_root / "run_exhausive_fourier.png")
+                torchvision.utils.save_image(perb_, output_root / "run_perturbation.png")
 
                 losses = return_dict["losses"]  # (B, H, W_)
                 losses /= torch.max(losses.view(batch_size, -1), dim=-1)[0][:, None, None]
-                torchvision.utils.save_image(losses[:, None, :, :].repeat(1, 3, 1, 1), output_root / "losses.png")
+                torchvision.utils.save_image(losses[:, None, :, :].repeat(1, 3, 1, 1), output_root / "run_losses.png")
 
                 errors = torch.ones_like(return_dict["corrects"]) - return_dict["corrects"]
-                torchvision.utils.save_image(errors[:, None, :, :].repeat(1, 3, 1, 1), output_root / "errors.png")
+                torchvision.utils.save_image(errors[:, None, :, :].repeat(1, 3, 1, 1), output_root / "run_errors.png")
 
                 assert perb.size() == torch.Size([batch_size, 3, 32, 32])
+                break  # test only first batch
+
+    def test__forward(self, pretrained_cifar10_resnet50, cifar10_stats, normalize_cifar10_loader):
+        input_size: Final[int] = 32
+        eps_max: Final[float] = 8.0
+        devices = {"cuda"}
+        output_root: Final = pathlib.Path("logs/test/")
+
+        model = pretrained_cifar10_resnet50
+        criterion_func = torch.nn.functional.cross_entropy
+
+        mean, std = cifar10_stats
+        for device in devices:
+            attacker = fourier_attack.attack.exhausive_fourier.ExhausiveFourierAttack(
+                input_size,
+                mean,
+                std,
+                eps_max,
+                criterion_func,
+                device
+            )
+
+            for x, t in normalize_cifar10_loader:
+                x, t = x.to(device), t.to(device)
+                batch_size = x.size(0)
+
+                x_adv = attacker(model, x, t)
+                torchvision.utils.save_image(x_adv, output_root / "forward_exhausive_fourier.png")
+
+                assert x_adv.size() == torch.Size([batch_size, 3, 32, 32])
                 break  # test only first batch
