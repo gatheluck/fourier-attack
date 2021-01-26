cffea2682bbfe103dceec2d4718f6d781800d931
gatheluck <gatheluck+tech@gmail.com>
2021-01-26 14:28:34 +0900

    test: add test code for exhausive fourier.

---
+++ tests/attack/test_exhausive_fourier.py
@@ -0,0 +1,47 @@
+import pathlib
+
+import torch
+import torchvision
+from typing import Final
+
+import fourier_attack.attack.exhausive_fourier
+
+
+class TestExhausiveFourierAttack:
+    def test__run(self, pretrained_cifar10_resnet50, cifar10_stats, denormalize_cifar10_loader):
+        devices = {"cuda"}
+        output_root: Final = pathlib.Path("logs/test/")
+
+        model = pretrained_cifar10_resnet50
+        criterion_func = torch.nn.functional.cross_entropy
+
+        mean, std = cifar10_stats
+        for device in devices:
+            pixel_model = fourier_attack.attack.PixelModel(model, 32, mean, std, device)
+
+            for x_denorm, t in denormalize_cifar10_loader:
+                x_denorm, t = x_denorm.to(device), t.to(device)
+                pixel_x = 255. * x_denorm
+
+                batch_size = pixel_x.size(0)
+                eps = 8. * torch.ones(batch_size).to(device)
+
+                sign_plus = torch.ones(batch_size)[:, None].repeat(1, 3).to(device)
+                channel_sign = torch.where(torch.rand_like(sign_plus) >= 0.5, sign_plus, -sign_plus)
+
+                perb, return_dict = fourier_attack.attack.exhausive_fourier.ExhausiveFourierAttack.run(pixel_model, pixel_x, t, eps, channel_sign, criterion_func)
+
+                x_adv = (pixel_x + perb) / 255.
+                perb_ = (perb / 255.) + 0.5
+                torchvision.utils.save_image(x_adv, output_root / "exhausive_fourier.png")
+                torchvision.utils.save_image(perb_, output_root / "perturbation.png")
+
+                losses = return_dict["losses"]  # (B, H, W_)
+                losses /= torch.max(losses.view(batch_size, -1), dim=-1)[0][:, None, None]
+                torchvision.utils.save_image(losses[:, None, :, :].repeat(1, 3, 1, 1), output_root / "losses.png")
+
+                errors = torch.ones_like(return_dict["corrects"]) - return_dict["corrects"]
+                torchvision.utils.save_image(errors[:, None, :, :].repeat(1, 3, 1, 1), output_root / "errors.png")
+
+                assert perb.size() == torch.Size([batch_size, 3, 32, 32])
+                break  # test only first batch
